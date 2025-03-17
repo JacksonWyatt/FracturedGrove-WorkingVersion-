941b3f73b5e1d2bafc1c9594e12eab83fd25615c
jSmith-Apollo <chad.smith@stu.daviess.kyschools.us>
2025-03-17 13:09:14 -0500

    Teleporters & Redone Prefabs

---
+++ Assets/Scripts/Objects/DebugObj.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DebugObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (EditorApplication.isPlaying || Application.isPlaying)
+        {
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+            renderer.enabled = false;
+        }
+
+        else return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Objects/Teleport.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject obj;
+
+    //public float x;
+    //public float y;
+    //public float z;
+    // Start is called before the first frame update
+    GameObject[] children;
+    Vector3 dest;
+    GameObject teleportObj;
+    void Start()
+    {
+        //Vector3 dest = new Vector3(x, y, z);
+        children = getDescendants(transform.gameObject);
+
+        dest = getObjectWithName(children, "Tele_Dest").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Entered");
+        teleportObj = collision.gameObject;
+        teleportObj.transform.position = dest;
+    }
+
+    public GameObject[] getDescendants(GameObject obj)
+    {
+        //return null if no object is present
+        if (obj == null) return null;
+        //Get list of all  transforms in the main children object//
+        Transform[] childrenT = obj.GetComponentsInChildren<Transform>();
+        GameObject[] children = new GameObject[childrenT.Length - 1];
+
+        //Add each object of the transforms to the childrens list
+        for (int i = 0; i < children.Length; i++)
+        {
+
+            children[i] = childrenT[i + 1].gameObject;
+            //print("" + children[i]);
+
+        }
+
+        return children;
+    }
+
+    public GameObject getObjectWithName(GameObject[] objects, string name)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].name == name)
+            {
+                return objects[i];
+            }
+        }
+        return null;
+    }
+}
